ee31a130f56e0b37d3836c3600da393acbf7e304
Kimyeonho <kyh0703@nate.com>
2022-12-29 17:55:37 +0900

    [Feature] Init messages project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
-@Controller('/app')
+@Controller()
 export class AppController {
-  @Get('/asdf')
-  getRootRoute() {
-    return 'hi there!';
-  }
+  constructor(private readonly appService: AppService) {}
 
-  @Get('/bye')
-  getByeThere() {
-    return 'bye there!';
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
