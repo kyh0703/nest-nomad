82d82007060d5d763e80d7e23c86cb96f0229fc4
Kimyeonho <kyh0703@nate.com>
2023-01-01 01:03:28 +0900

    [Feature] DI Short Project

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-import { ValidationPipe } from '@nestjs/common';
-import { MessagesModule } from './messages/messages.module';
 import { NestFactory } from '@nestjs/core';
+import { ComputerModule } from './computer/computer.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MessagesModule);
-  app.useGlobalPipes(new ValidationPipe());
+  const app = await NestFactory.create(ComputerModule);
   await app.listen(3000);
 }
 bootstrap();
