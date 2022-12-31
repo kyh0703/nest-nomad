441c240183933354baea0413a6f9ff690528d59a
Kimyeonho <kyh0703@nate.com>
2022-12-31 14:52:58 +0900

    [Feature] Message INit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MessagesController } from './messages/messages.controller';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MessagesModule],
+  controllers: [MessagesController],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // Service is Creating its own dependencies
+    // DONT DO THIS ON REAL APPS
+    this.messagesService = new MessagesService();
+  }
+
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    return this.messagesService.findOne(id);
+  }
+}
