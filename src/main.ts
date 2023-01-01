ba8a93153fd7c439f02b7bad968ed2d1d97fa342
Kimyeonho <kyh0703@nate.com>
2023-01-01 23:53:10 +0900

    [Feature] Entity ing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // 지정하지 않은 dto 데이터는 삭제
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
